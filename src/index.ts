1721de66ad495b786d483c30f7215305904678f1
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2023-10-06 19:59:47 +0200

    add to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { getDocuments } from './getDocuments';
+export { getParties } from './getParties';
+export { postParty } from './postParty';
 
 export type { Document } from './types/document';
 export type { Party } from './types/party';
