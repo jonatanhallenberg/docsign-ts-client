ca5d0d3e18483844f3fcf724465da6781c6b3f41
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2023-10-10 12:59:24 +0200

    add headers to post party

--- src/postParty.ts
+++ src/postParty.ts
@@ -1,7 +1,11 @@
 import { Party } from "./types/party";
 import { PostPartyResponse } from "./types/postPartyResponse";
 
-export const postParty = async (apiKey: string, party: Party, baseUrl: string = "https://docsign.se/api/", ): Promise<PostPartyResponse> => {
-    const response = await fetch(`${baseUrl}parties?api_key=${apiKey}`, { method: "POST", body: JSON.stringify(party) });
+export const postParty = async (apiKey: string, party: Party, baseUrl: string = "https://docsign.se/api/",): Promise<PostPartyResponse> => {
+    const response = await fetch(`${baseUrl}parties?api_key=${apiKey}`, {
+        method: "POST",
+        headers: new Headers({ 'content-type': 'application/json' }),
+        body: JSON.stringify(party)
+    });
     return (await response.json()) as PostPartyResponse;
 }
