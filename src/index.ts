7340124826cc8183fafe0b0fa959ba2d44adfc36
Mathilda Fritzell <mathilda.fritzell@gmail.com>
2025-02-14 16:17:28 +0100

    lagt till exporter för postDocument och postResponses

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { getDocuments } from './getDocuments';
 export { getParties } from './getParties';
 export { postParty } from './postParty';
-
-export type { Document } from './types/document';
+export { postDocument } from './postDocument';
+export type { Document, DocumentAttachment, CreateDocumentRequest } from './types/document';
 export type { Party } from './types/party';
+export type { PostPartyResponse, PostDocumentResponse } from './types/postResponse';
---
+++ src/types/postResponse.ts
@@ -0,0 +1,11 @@
+
+export type PostPartyResponse = {
+    success: boolean;
+    message: string;
+    "party_id": number;
+}
+export type PostDocumentResponse = {
+    success: boolean;
+    message: string;
+    "document_id": number;
+}
